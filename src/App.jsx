24ada2615274cd0f28cc20fc0d1d3d67ea4a65dd
Muhammad Ahmad <ahmadwebcraft@gmail.com>
2023-09-11 23:59:20 +0500

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React, { lazy, Suspense } from 'react';
-
-const Hero = lazy(() => import('./components/Hero'));
-const Header = lazy(() => import('./components/Header'));
-const Portfolio = lazy(() => import('./components/Portfolio'));
-const Services = React.lazy(() => import('./components/Services'));
-const Projects = React.lazy(() => import('./components/Projects'));
-const Choose = React.lazy(() => import('./components/Choose'));
-const Clients = React.lazy(() => import('./components/Clients'));
-const Form = React.lazy(() => import('./components/Form'));
-const Footer = React.lazy(() => import('./components/Footer'));
+import React from 'react';
+import Hero from './components/Hero';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Services from './components/Services';
+import Projects from './components/Projects';
+import Choose from './components/Choose';
+import Clients from './components/Clients';
+import Form from './components/Form';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -18,7 +17,6 @@ const App = () => {
         <Hero />
 
       </div>
-      <Suspense fallback={<div className='bg-backgroung-gradient'>Loading...</div>}>
         <div className='relative text-white'>
           <Portfolio />
           <div id='services'><Services /></div>
@@ -28,7 +26,6 @@ const App = () => {
           <Form />
           <Footer />
         </div>
-      </Suspense>
 
     </div>
   );
